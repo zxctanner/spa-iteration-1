24893e0a5ab353a6716c389f5498c1d069da32b1
Jeremy <jomsenong@hotmail.com>
2015-08-27 15:03:55 -0700

    PQL classes
    in progress

--- CS3201SPA/CS3201SPA/QueryEvaluator.cpp
+++ CS3201SPA/CS3201SPA/QueryEvaluator.cpp
@@ -1,12 +1,18 @@
 #include "QueryEvaluator.h"
+using namespace std;
 
 
 
-QueryEvaluator::QueryEvaluator()
-{
+QueryEvaluator::QueryEvaluator(){
 }
 
+QueryEvaluator::QueryEvaluator(String command) {
+	String a = null;
+	String b = null;
+	int first = null;
+	int second = null;
+}
 
-QueryEvaluator::~QueryEvaluator()
-{
+int QueryEvaluator::Modifies(String, String) {
+
 }
--- CS3201SPA/CS3201SPA/QueryEvaluator.h
+++ CS3201SPA/CS3201SPA/QueryEvaluator.h
@@ -2,7 +2,18 @@
 class QueryEvaluator
 {
 public:
-	QueryEvaluator();
-	~QueryEvaluator();
+	int Modifies(String, String);
+	String Modifies(int, String);
+
+	int Uses(String, String);
+	String Uses(int, String);
+
+	int Parent(int, int);
+	int ParentS(int, int);
+
+	int Follows(String, int);
+	int Follows(int, String);
+	int FollowsS(String, int);
+	int FollowsS(int, String);
 };
 
