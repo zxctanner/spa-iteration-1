938a0070a43ab2a34a04ce5c416e9ce84a2abc6d
Houyi91 <a0096614@u.nus.edu>
2015-08-31 10:52:21 +0800

    AST Class

--- CS3201SPA/CS3201SPA/AST.h
+++ CS3201SPA/CS3201SPA/AST.h
@@ -1,7 +1,22 @@
-#pragma once
-class AST
-{
-public:
-	AST();
-};
+#include "TNode.h"
 
+class AST {
+public:
+	//Constructor with root 
+	AST(TNode root);
+	TNode getRoot();
+	void addParent(TNode parent);
+	void addChild(TNode child);
+	void addLeftSibling(TNode leftSibling);
+	void addRightSibling(TNode rightSibling);
+	//Traversing the AST
+	void gotoParent();
+	void gotoChild();
+	void gotoLeftSibling();
+	void gotoRightSibling();
+	//Reading node
+	string getName();
+	string getNodeType();
+	int getStmtNum();
+
+};
