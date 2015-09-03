a2a665649d965d5002ccdc6a2079095eedecbc0b
Houyi91 <a0096614@u.nus.edu>
2015-09-04 02:30:25 +0800

    Added parent*  table get function in PKB

--- CS3201SPA/CS3201SPA/PKB.cpp
+++ CS3201SPA/CS3201SPA/PKB.cpp
@@ -9,6 +9,7 @@ class PKB
 {
 private:
 	vector<pair<int, int>> parentTable;
+	vector<pair<int, int>> parentStarTable;
 	unordered_map<int, pair<string, string>> modUseTable;
 	unordered_map<int, LineToken*> statementTable;
 	vector<pair<int, int>> followTable;
@@ -16,6 +17,7 @@ private:
 public:
 	void constructAllTables(vector<LineToken> tokens);
 	vector<pair<int, int>> getParentTable();
+	vector<pair<int, int>> getParentStarTable();
 	unordered_map<int, pair<string, string>> getmodUseTable();
 	unordered_map<int, LineToken*> getStatementTable();
 	vector<pair<int, int>> getFollowTable();
@@ -29,6 +31,11 @@ void PKB::constructAllTables(vector<LineToken> tokens)
 vector<pair<int, int>> PKB::getParentTable()
 {
 	return parentTable;
+}
+
+vector<pair<int, int>> PKB::getParentStarTable()
+{
+	return parentStarTable;
 }
 
 unordered_map<int, pair<string, string>> PKB::getmodUseTable()
